3193f547fcdd3fd7a82ddcce5f08b33b5e66662a
John Murray <jmurrayufo@gmail.com>
2019-04-04 22:34:13 -0600

    Track if host is a VM from mac address guess

--- StatsTest.py
+++ StatsTest.py
@@ -3,6 +3,7 @@ import requests
 import psutil
 import socket
 import time
+import uuid
 
 influxDB_host = "http://192.168.3.4:8086"
 
@@ -19,37 +20,39 @@ while 1:
     # t_last = time.time()
 
     data = ""
+    hostname = socket.gethostname()
+    is_vm = f"{uuid.getnode():012X}".startswith("080027")
 
     # Measure CPU %'s
     cpu_percents = psutil.cpu_percent(interval=min_time_between_reports-1, percpu=True)
     for idx,core in enumerate(cpu_percents):
-        data +=  f"cpu,hostname={socket.gethostname()},core={idx} use={cpu_percents[idx]}\n"
+        data +=  f"cpu,hostname={hostname},is_vm={is_vm},core={idx} use={cpu_percents[idx]}\n"
 
     cpu_freqs = psutil.cpu_freq(percpu=True)
     for idx,core in enumerate(cpu_freqs):
-        data +=  f"cpu,hostname={socket.gethostname()},core={idx} freq={cpu_freqs[idx].current}\n"
+        data +=  f"cpu,hostname={hostname},is_vm={is_vm},core={idx} freq={cpu_freqs[idx].current}\n"
 
     # Measure Memory
     mem_data = psutil.virtual_memory()
-    data += f"memory,hostname={socket.gethostname()} total={mem_data.total}\n"
-    data += f"memory,hostname={socket.gethostname()} available={mem_data.available}\n"
-    data += f"memory,hostname={socket.gethostname()} percent={mem_data.percent}\n"
-    data += f"memory,hostname={socket.gethostname()} used={mem_data.used}\n"
-    data += f"memory,hostname={socket.gethostname()} free={mem_data.free}\n"
-    data += f"memory,hostname={socket.gethostname()} active={mem_data.active}\n"
-    data += f"memory,hostname={socket.gethostname()} inactive={mem_data.inactive}\n"
-    data += f"memory,hostname={socket.gethostname()} buffers={mem_data.buffers}\n"
-    data += f"memory,hostname={socket.gethostname()} cached={mem_data.cached}\n"
-    data += f"memory,hostname={socket.gethostname()} shared={mem_data.shared}\n"
-    data += f"memory,hostname={socket.gethostname()} slab={mem_data.slab}\n"
+    data += f"memory,hostname={hostname},is_vm={is_vm} total={mem_data.total}\n"
+    data += f"memory,hostname={hostname},is_vm={is_vm} available={mem_data.available}\n"
+    data += f"memory,hostname={hostname},is_vm={is_vm} percent={mem_data.percent}\n"
+    data += f"memory,hostname={hostname},is_vm={is_vm} used={mem_data.used}\n"
+    data += f"memory,hostname={hostname},is_vm={is_vm} free={mem_data.free}\n"
+    data += f"memory,hostname={hostname},is_vm={is_vm} active={mem_data.active}\n"
+    data += f"memory,hostname={hostname},is_vm={is_vm} inactive={mem_data.inactive}\n"
+    data += f"memory,hostname={hostname},is_vm={is_vm} buffers={mem_data.buffers}\n"
+    data += f"memory,hostname={hostname},is_vm={is_vm} cached={mem_data.cached}\n"
+    data += f"memory,hostname={hostname},is_vm={is_vm} shared={mem_data.shared}\n"
+    data += f"memory,hostname={hostname},is_vm={is_vm} slab={mem_data.slab}\n"
 
     # Measure Disks
     disk_use = psutil.disk_usage("/")
-    data += f"disk,hostname={socket.gethostname()} total={disk_use.total},used={disk_use.used},free={disk_use.free},percent={disk_use.percent}\n"
+    data += f"disk,hostname={hostname},is_vm={is_vm} total={disk_use.total},used={disk_use.used},free={disk_use.free},percent={disk_use.percent}\n"
 
     # Measure Processes
     num_pids = len(psutil.pids())
-    data += f"pids,hostname={socket.gethostname()} count={num_pids}\n"
+    data += f"pids,hostname={hostname},is_vm={is_vm} count={num_pids}\n"
 
     # Measure Temperatures
     temperatures = psutil.sensors_temperatures()
@@ -59,7 +62,10 @@ while 1:
                 label = 'none'
             else:
                 label = obj.label
-            data += f"temperature,hostname={socket.gethostname()},chipset={name},val_index={idx},label={label} current={obj.current}\n"
+            data += f"temperature,hostname={hostname},is_vm={is_vm},chipset={name},val_index={idx},label={label} current={obj.current}\n"
+
+    # Detect MAC for host type identication
+
 
     host = influxDB_host + '/write'
     params = {"db":"systems","precision":"s"}
